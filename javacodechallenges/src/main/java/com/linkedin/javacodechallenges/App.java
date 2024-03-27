67d67dca1ddb533ecc166fd3105be7c060c690cd
Oludayo Adeoye <49393855+dbillion@users.noreply.github.com>
2024-03-27 16:21:02 +0000

    implment calculate water bill

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,8 +5,19 @@ import java.util.Scanner;
 public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+
+        double minimumWaterBill=18.84;
+        double numberOfGallonsInCCF=748;
+        double IncludedGallons = 2 * numberOfGallonsInCCF;
+
+        if (gallonsUsage<= IncludedGallons) {
+            return minimumWaterBill;
+        }else{
+            double extraGallonsUsed=gallonsUsage -IncludedGallons;
+            double extraAmountOwed =Math.ceil(extraGallonsUsed/numberOfGallonsInCCF)*3.9;
+            return minimumWaterBill + extraAmountOwed;
+        }
+       
     }
 
     public static void main(String[] args) {
